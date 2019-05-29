28afd6453c5e9a3fe3f158a69fe503db6e74e6be
Kirby Steckel <steckelk@southwestern.edu>
2019-05-29 11:10:31 -0500

    comments added to python files

--- models/user_model.py
+++ models/user_model.py
@@ -1,26 +1,31 @@
 from db import db
 
 
+# class that models a user
 class UserModel(db.Model):
 
-    __tablename__ = "users"
+    __tablename__ = "users"  # table name is users
 
-    id = db.Column(db.INTEGER, primary_key=True)
-    username = db.Column(db.String(40))
-    password = db.Column(db.String(40))
+    id = db.Column(db.INTEGER, primary_key=True)  # column in table for id for user, auto incremented
+    username = db.Column(db.String(40))  # column in table for the user's username that is entered at login, no repeats
+    password = db.Column(db.String(40))  # column in table for the user's password that is entered at login, no repeats
 
+    # initializes instance of UserModel with the data for all columns given as parameters
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
+    # saves the UserModel to the database
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
 
+    # finds a row by specific username given as a parameter
     @classmethod
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
 
+    # finds a row by specific id given as a parameter
     @classmethod
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
--- security.py
+++ security.py
@@ -2,12 +2,14 @@ from models.user_model import UserModel
 from werkzeug.security import safe_str_cmp
 
 
+# authenticates user for security
 def authenticate(username, password):
-    user = UserModel.find_by_username(username)
+    user = UserModel.find_by_username(username)  # returns user with specified username
+    # if user and password are true then return the user
     if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return UserModel.find_by_id(user_id)
+    return UserModel.find_by_id(user_id) # returns user based on their id, or None if not found
